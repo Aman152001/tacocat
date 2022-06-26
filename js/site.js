bf58bae3c86a3d79fdb56de66671e76de2fbe82e
Aman152001 <amanpyadav15@gmail.com>
2022-06-26 16:28:19 -0400

    Done with the taco cat project!

--- js/site.js
+++ js/site.js
@@ -1,36 +1,38 @@
+
 function getValue(){
     document.getElementById("alert").classList.add("invisible");
    let userString =  document.getElementById("userString").value;
+   userString = userString.toLowerCase(userString);
+   //remove space and special characters.
+   let regex = /[^a-z0-9]/gi;
+   userString = userString.replace(regex,"");
    let returnObj = checkForPalindrome(userString);
-   display(returnObj)
+   display(returnObj,userString);
 }
 
 //check if a given string is palindrome or not!
 function checkForPalindrome(userString){
 
-    userString = userString.toLowerCase(userString);
-    //remove space and special characters.
-
-    let regex = /[^a-z0-9]/gi;
-    userString = userString.replace(regex,"");
     let revString = [];
-    let returnObj = {};
     for( let i =userString.length-1;i>=0;i--)
 {
     revString+=userString[i];
 } 
-if (revString== userString){
-    returnObj.msg="Well Done! You entered a Palindrome!"
-}
-else {
-    returnObj.msg= "Sorry! You did not entered a Palindrome!"
-}
-returnObj.reversed = revString;
-return returnObj;
+
+return revString;
 }
 
-function display(returnObj){
-    document.getElementById("alertHeader").innerHTML = returnObj.msg;
-    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
-    document.getElementById("alert").classList.remove("invisible");
+function display(returnObj,userString){
+    if(returnObj==userString){
+        document.getElementById("alert").classList.add("alert-success");
+        document.getElementById("msg").innerHTML = `Congrats it's a palindrome!`;
+        document.getElementById("alert").classList.remove("invisible");
+        document.getElementById("alert").classList.remove("alert-danger");
+    }
+    else{
+        document.getElementById("alert").classList.add("alert-danger");
+        document.getElementById("msg").innerHTML = `Sorry it's not a palindrome!`;
+        document.getElementById("alert").classList.remove("invisible");
+        document.getElementById("alert").classList.remove("alert-success");
+    }
 }
